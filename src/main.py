c53a3baa15829d21025bbaac21939a74dd6b3884
Pridewolf <agus_blackarrow_00@hotmail.com>
2024-01-12 16:59:16 -0300

    Add Changes de code

--- src/main.py
+++ src/main.py
@@ -6,26 +6,30 @@ import numpy as np
 import pandas as pd 
 
 import tensorflow as tf
-
 from tensorflow import keras
 import keras_cv
 
+# Decorador para caché
+@st.cache(allow_output_mutation=True)
+def cargar_modelo():
+    # Crear y cargar el modelo
+    return keras_cv.models.StableDiffusion(img_width=512, img_height=512)
 
+def main():
+    try:
+        st.title('Generador de Imagen por Prompt')
 
-def main() :
-    try : 
-        st.title('Genereador de Imagen por prompt')
-
-        st.write('Escribe lo que deseas generar a traves del modelo generador de imagenes ')
+        st.write('Escribe lo que deseas generar a través del modelo generador de imágenes')
         text_image_generator = st.text_input('Prompt to Create Images', 'Horse on the rainbow')
 
-        model = keras_cv.models.StableDiffusion(img_width=512, img_height=512)
+        model = cargar_modelo()
 
-        images = model.text_to_image(text_image_generator, batch_size=1)
+        with st.spinner("Generando imágenes..."):
+            images = model.text_to_image(text_image_generator, batch_size=1)
 
         plot_images(images)
-    except Exception as e :
-        print(e)
+    except Exception as e:
+        st.error(f"Error: {e}")
 
 def plot_images(images):
     for i in range(len(images)):
